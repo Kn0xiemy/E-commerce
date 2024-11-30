06e04b974ae579e444c6b75b56229bc540086d26
Kn0xiemy <112770389+Kn0xiemy@users.noreply.github.com>
2024-11-30 22:17:06 +0200

    added routing & another page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AuthServicesComponent } from './auth-services/auth-services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HomeComponent,
+    path:""
+  },
+  {
+    component:AuthServicesComponent,
+    path:"seller-authorization"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
